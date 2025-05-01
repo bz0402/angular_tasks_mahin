72c7208598c43b05867e6c0d0aeed95170e5af49
MahinSojitra <mahin.sojitra@outlook.com>
2025-05-01 16:58:42 +0530

    create common layout.

---
+++ news-scope-app/src/app/layout/layout.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainLayoutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    MainLayoutComponent,
+  ]
+})
+export class LayoutModule { }
